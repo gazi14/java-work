047a9f38cb3a7331050881e3b43f1d116145057d
gazi14 <asif.gazi95@gmail.com>
2015-12-24 19:15:37 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////////////////////////
+//Gazi Asif
+//Welcome Class Java Program
+//
+    public class WelcomeClass {
+        public static void main(String[] args) {
+            System.out.println("-----------");
+            System.out.println("| WELCOME |");
+            System.out.println("-----------");
+            System.out.println(" ^  ^  ^  ^  ^  ^ ");
+            System.out.println("\ \/ \/ \/ \/ \/ \");
+            System.out.println("| -.--A--S--I--F--.- |");
+            System.out.println("\ /\ /\ /\ /\ /\ /");
+            System.out.println(" v  v  v  v  v  v ");
+            
+            
+        }
+    }
