8582988583570750bf87d37a49c87c81493feb6f
gazi14 <asif.gazi95@gmail.com>
2015-12-24 20:46:45 +0000

    updated Arithmetic

---
+++ hw02/Arithmetic.java
@@ -0,0 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////////
+//Gazi Asif
+//Arithmetic Java Program
+//
+
+    public class Arithmetic {
+        public static void main(String[] args) {
+            int nSocks=3;                           //Number of pairs of socks
+            double sockCost$=2.58;                  //Cost per pair of socks
+            int nGlasses=6;                         //Number of drinking glasses
+            double glassCost$=2.29;                 //Cost per glass
+            int nEnvelopes=1;                       //Number of boxes of envelopes
+            double envelopeCost$=3.25;              //Cost per box of envelopes
+            double taxPercent=0.06;
+            double totalSockCost$;                  //Total cost of socks
+            
+            System.out.println("First item: Pairs of Socks. 3 are being bought and they each cost $2.58.");
+            System.out.println("Second item: Drinking glasses. 6 are being bought and they each cost $2.29.");
+            System.out.println("Third item: Boxes of envelopes. 1 is being bought and it costs $3.25. ");
+            
+            
+        }
+    }
